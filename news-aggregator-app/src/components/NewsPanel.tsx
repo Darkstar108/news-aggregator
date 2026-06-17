471d311c60f548260380368cf755a8b7fc5c9241
Pranav Rai <pranavrai108@gmail.com>
2026-06-17 19:27:31 +0530

    Add ScrollToTop component

--- news-aggregator-app/src/components/NewsPanel.tsx
+++ news-aggregator-app/src/components/NewsPanel.tsx
@@ -1,6 +1,7 @@
 import Stack from "@mui/material/Stack";
 import NewsCard from "./NewsCard";
 import type { NewsItem } from "../types/NewsAggregator";
+import ScrollToTop from "react-scroll-to-top";
 
 type NewsPanelProps = {
   newsItems: NewsItem[];
@@ -15,6 +16,7 @@ export default function NewsPanel({ newsItems = [] }: NewsPanelProps) {
       <Stack direction="column" spacing={2}>
         {newsCards}
       </Stack>
+      <ScrollToTop style={{ right: 30, bottom: 30 }} />
     </div>
   );
 }
